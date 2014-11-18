82b16f9763f80fbce9d73bc3a79f8a61031d97b6
Alan Fong <allank.fong@gmail.com>
2014-11-18 11:32:58 -0600

    Store

---
+++ nopTest/MvcStores/Controllers/PhoneBookController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStores.Models;
+
+namespace MvcStores.Controllers
+{
+    public class PhoneBookController : Controller
+    {
+
+        private static List<PhoneBookModel> _PhoneBookList;
+
+        public static List<PhoneBookModel> PhoneBookList
+        {
+            get
+            {
+                if (_PhoneBookList == null)
+                {
+                    _PhoneBookList = new List<PhoneBookModel>();
+                    _PhoneBookList.Add(new PhoneBookModel
+                    {
+                        FirstName = "Alan",
+                        LastName = "Fong",
+                        MiddleName="Kent",
+                        MobileNumber = "204 255 5266",
+                        Email = "allank.fong@gmail.com"
+                    });
+                }
+                    return _PhoneBookList;
+            }
+            set { _PhoneBookList = value; }
+        }
+        //
+        // GET: /PhoneBook/
+
+        public ActionResult Index()
+        {
+            return View(PhoneBookList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ nopTest/MvcStores/Models/PhoneModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcStores.Models
+{
+    public class PhoneBookModel
+    {
+        [Required]
+        [Display(Name="First Name")]
+        [StringLength(50, ErrorMessage="This {0} must be at least {2} characters", MinimumLength=6)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Midlle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile")]
+        public string MobileNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+    }
+}
